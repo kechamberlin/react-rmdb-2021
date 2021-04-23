c820fc6e6d89c29797b070318199f6930fdf3739
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-04-23 14:46:37 -0700

    Imported BrowserRouter, Routes, and Route from react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import { GlobalStyle } from './GlobalStyle';
