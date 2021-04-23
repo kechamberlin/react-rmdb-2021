3aa614c5ebb0fa195787cc4789c191051e98c808
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-04-23 14:41:13 -0700

    Imported Link from react-router-dom.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
