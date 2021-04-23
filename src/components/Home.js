db24c71b8f90b6e63a106f89f10d8fde1b3c3c97
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-04-23 12:16:01 -0700

    Created Home Component that uses useHomeFetch Custom Hook and displays the HeroImage Component.

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import { useHomeFetch } from '../hooks/useHomeFetch';
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+import HeroImage from './HeroImage';
+import NoImage from '../images/no_image.jpg';
+
+export default function Home() {
+  const { state, loading, error } = useHomeFetch();
+
+  console.log(state);
+
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
+}
