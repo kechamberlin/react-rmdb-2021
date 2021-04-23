db2f89a8d24c14752338ed770c736ddc1b72a11b
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-04-23 13:33:45 -0700

    Created seachTerm state set to empty string.

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -9,6 +9,7 @@ const initialState = {
 };
 
 export function useHomeFetch() {
+  const [searchTerm, setSearchTerm] = useState('');
   const [state, setState] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
